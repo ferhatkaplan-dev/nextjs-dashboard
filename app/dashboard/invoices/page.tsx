7afab1bd56c2b707c1cfb5c00dd670cff52d236a
Ferhat Kaplan <fkmll2121@gmail.com>
2024-07-28 14:10:21 +0300

    Updated metadata for pages, removed favicon and opengraph-image files, and made minor code formatting changes.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,18 +1,23 @@
-import Pagination from '@/app/ui/invoices/pagination';
-import Search from '@/app/ui/search';
-import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
-import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
-import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from "next";
+import Pagination from "@/app/ui/invoices/pagination";
+import Search from "@/app/ui/search";
+import Table from "@/app/ui/invoices/table";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { lusitana } from "@/app/ui/fonts";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import { fetchInvoicesPages } from "@/app/lib/data";
+
+export const metadata: Metadata = {
+  title: "Lorem Invoices",
+};
 
 export default async function Page({
   searchParams,
 }: {
   searchParams?: { query?: string; page?: string };
 }) {
-  const query = searchParams?.query || '';
+  const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
 
   const totalPages = await fetchInvoicesPages(query);
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next";
 import AcmeLogo from "@/app/ui/acme-logo";
 import LoginForm from "@/app/ui/login-form";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
 
 export default function LoginPage() {
   return (
@@ -8,6 +13,7 @@ export default function LoginPage() {
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
             <AcmeLogo />
+            fff
           </div>
         </div>
         <LoginForm />
